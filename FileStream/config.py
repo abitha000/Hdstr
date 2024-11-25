cb2196580b9e5f98d165b529c2e738ff8031d9b2
abitha000 <abithak000@yahoo.com>
2024-11-25 19:31:02 +0530

    Update config.py

--- FileStream/config.py
+++ FileStream/config.py
@@ -33,7 +33,7 @@ class Server:
     HAS_SSL = str(env.get("HAS_SSL", "0").lower()) in ("1", "true", "t", "yes", "y")
     NO_PORT = str(env.get("NO_PORT", "0").lower()) in ("1", "true", "t", "yes", "y")
     FQDN = str(env.get("FQDN", BIND_ADDRESS))
-    URL = "https://victorious-matti-deployingbots-da9f88bf.koyeb.app/".format(
+    URL = "https://web-tjsrlkx0c4go.up-de-fra1-k8s-1.apps.run-on-seenode.com/".format(
         "s" if HAS_SSL else "", FQDN, "" if NO_PORT else ":" + str(PORT)
     )
 
